d3326c71dca7aa0542b66b00b8ecbaaaa6b256bb
yossonline <yossonl34@gmail.com>
2013-10-10 13:00:34 -0500

    Cambios realizados fuera
    Cambios realizados fuera

--- MvcMovie/MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -24,6 +24,7 @@ namespace MvcMovie.Controllers
 
         public ActionResult Contact()
         {
+            int valor = 0;
             ViewBag.Message = "Your contact page.";
 
             return View();
